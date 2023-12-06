84d6f2024a082d61e26882f09a2670892aa7c293
Emmanuel Selby <fetz.selby@gmail.com>
2023-12-06 15:12:55 +0100

    styles added

---
+++ src/styles/index.ts
@@ -0,0 +1,14 @@
+const colors = {
+  typography: {
+    big: '#555555',
+    medium: '#777777',
+    small: '#999999',
+    light: '#fefefe',
+  },
+  layout: {
+    main: '#ffffff',
+    secondary: '#000000',
+  },
+};
+
+export { colors };
