04616771115e1c4a7978f4cc23eb422291ae0c04
Emmanuel Selby <fetz.selby@gmail.com>
2023-12-06 15:15:38 +0100

    empty user component added

---
+++ src/components/EmptyUser/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+const EmptyUser = () => (
+  <Typography data-testid="no-user-available" variant="body1">
+    Oops! No user(s) available
+  </Typography>
+);
+
+export { EmptyUser };
